950839dd54dbcbcdf9ff7a456c65bc0a510e3fc1
EliNygard <eli.nygard@gmail.com>
2025-02-12 16:59:19 +0100

    installs and configure tailwind

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,8 +1,14 @@
+import { AiOutlineShopping } from "react-icons/ai";
+
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        
+        <div className="flex justify-between">
+            <h1 className="text-2xl">Header</h1>
+        <div className="cart-icon-container">
+            <span>
+            <AiOutlineShopping />
+            </span>
+        </div>
         </div>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export const content = [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // ensure jsx files are included
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
