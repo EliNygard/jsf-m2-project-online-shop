4f8a8dee8bfc4e354bbde373baa6354cd225232e
EliNygard <eli.nygard@gmail.com>
2025-02-12 18:31:21 +0100

    adds header component

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,13 +1,19 @@
 import { AiOutlineShopping } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+    // const products = useSelector((state) => state.cart.products);
+    const cartQuantity = useSelector((state) => state.cart.cartQuantity)
+
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center mb-4">
             <h1 className="text-2xl">Header</h1>
         <div className="cart-icon-container">
-            <span>
-            <AiOutlineShopping />
+            <span className="cart-icon">
+            <AiOutlineShopping className="text-purple-900 w-full text-3xl" />
             </span>
+            <span className="text-xs">Shopping cart</span>
+            <span className="overlay">{cartQuantity}</span>
         </div>
         </div>
     )
